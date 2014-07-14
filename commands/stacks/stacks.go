54e6d47ddb62040fb9b67ceee32fd7a834f691fc
Brett Wilkins <brett@brett.geek.nz>
2014-07-14 19:54:36 +1000

    Finishing slow stacks command

--- commands/stacks/stacks.go
+++ commands/stacks/stacks.go
@@ -2,6 +2,7 @@ package stacks
 
 import (
   "log"
+  "strconv"
 
   "github.com/bwilkins/toolbelt/command"
   "github.com/bwilkins/toolbelt/encoding/term_table"
@@ -28,7 +29,24 @@ func runStacks(cmd *command.Command, args []string) {
 
   table := term_table.NewTermTable("Name", "Region", "# Layers", "# Instances", "ID")
   for _, stack := range stacksResponse.Stacks {
-    table.WriteRow(stack.Name, stack.Region, "layers", "instances", stack.StackId)
+    var layersCount, instanceCount int
+    layersResponse, err := opsworks.DescribeLayers(opsworks.DescribeLayersRequest{ StackId: stack.StackId })
+    if err != nil {
+      layersCount = 0
+    } else {
+      layersCount = len(layersResponse.Layers)
+    }
+    instancesResponse, err := opsworks.DescribeInstances(opsworks.DescribeInstancesRequest{ StackId: stack.StackId })
+    if err != nil {
+      instanceCount = 0
+    } else {
+      instanceCount = len(instancesResponse.Instances)
+    }
+
+    layers := strconv.FormatInt(int64(layersCount), 10)
+    instances := strconv.FormatInt(int64(instanceCount), 10)
+
+    table.WriteRow(stack.Name, stack.Region, layers, instances, stack.StackId)
   }
 
   table.PrintTable()
