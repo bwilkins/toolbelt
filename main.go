483b7baef549d0fbc9679d4e71cf0ccb3c4bc9ed
Brett Wilkins <brett@brett.geek.nz>
2014-09-02 11:15:18 +1000

    Adding check around args being passed. adding IntelliJ to the ignore file since I'm trialing it.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
   "os"
   "os/user"
   "path"
+  "fmt"
 
   "github.com/bwilkins/aws"
   "github.com/bwilkins/toolbelt/config"
@@ -16,8 +17,12 @@ func main() {
   config.SetConfig(path.Join(usr.HomeDir, ".toolbelt.yml"))
   aws.SetAccessCredentials( aws.Credentials{config.Config.OpsWorks.AccessId, config.Config.OpsWorks.SecretKey} )
 
-  commandName := os.Args[1]
-  commandArgs := os.Args[2:]
+  if len(os.Args) > 1 {
+    commandName := os.Args[1]
+	commandArgs := os.Args[2:]
 
-  commands.RunCommand(commandName, commandArgs)
+	commands.RunCommand(commandName, commandArgs)
+  } else {
+	fmt.Println("No command given, aborting.")
+  }
 }
